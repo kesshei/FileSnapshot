90aa31c5213b21181fb0cfe4bae408b12c24223f
kesshei <dagang520@qq.com>
2023-05-25 16:24:41 +0800

    修改信息

---
+++ FileSnapshot/FileSnapshot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileSnapshot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
